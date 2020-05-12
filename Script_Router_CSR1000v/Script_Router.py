8610535baf39b60a4095df2e8a616ae3bf43f3ae
Hector N Mesa <36802342+SaebaFox@users.noreply.github.com>
2020-05-12 01:25:08 +0100

    Update Script_Router.py

---
+++ Script_Router_CSR1000v/Script_Router.py
@@ -0,0 +1,102 @@
+from ncclient import manager
+import xmltodict
+import xml.dom.minidom
+
+Conec = manager.connect(
+   host= "192.168.56.101",
+   port= 830,
+   username = "cisco",
+   password = "cisco123!",
+   hostkey_verify = False
+)
+
+def get_info_device():
+
+   netconf_reply = Conec.get_config(source="running")
+   print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+
+def create_interface():
+   netconf_data = """
+   <config>
+      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+        <interface>
+            <Loopback>
+                <name>7</name>
+                <description>prueba</description>
+                <ip>
+                    <address>
+                        <primary>
+                            <address>192.168.60.60</address>
+                            <mask>255.255.255.0</mask>
+                        </primary>
+                    </address>
+                </ip>
+            </Loopback>
+        </interface>
+    </native>
+   </config>
+   """
+   netconf_reply = Conec.edit_config(target="running",config=netconf_data)
+   print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+def delete_interface():
+   netconf_data= """
+     <config>
+         <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+             <interface operation="delete">
+                 <name>7</name>
+             </interface>
+         </interfaces>
+     </config>"""
+   netconf_reply = Conec.edit_config(target="running",config=netconf_data)
+   print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+
+'''
+def get_table_routing():
+   filtro = """
+        <filter>
+             <routing-state xmlns="urn:ietf:params:xml:ns:yang:ietf-routing">
+                <routing-instance>
+                    <name>default</name>
+                    <ribs>
+                        <rib>
+                            <name>ipv4-default</name>
+                        </rib>
+                    </ribs>
+                </routing-instance>
+            </routing-state>
+        </filter>
+        """
+   Datos = Conec.get(filtro)
+   diccionario = xmltodict.parse(Datos.xml)
+
+   for item in diccionario:
+      
+'''
+
+def menu():
+   print("**********************")
+   print("Introduce la operacion")
+   print("1 get_info_device ")
+   print("2 create_interface")
+   print("3 delete_interface")
+   print("4 get_table_routing")
+   print("**********************")
+   return input("")
+
+def opcion(opcion):
+   if (opcion == "1"):
+      get_info_device()
+   elif(opcion == "2"):
+      create_interface()
+   elif(opcion == "3"):
+      delete_interface()
+   elif(opcion == "4"):
+      get_table_routing()
+   else:
+      print("opcion no valida")
+
+if __name__ == "__main__":
+   opcion(menu())
